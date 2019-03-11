59d1d8b3595fb6571b24a7d781c6d4937f9b2040
tvattima <github@vattima.com>
2019-03-11 19:58:18 -0400

    - Introduced initial lego-imaging commit

---
+++ src/main/java/com/vattima/lego/imaging/config/LegoImagingConfiguration.java
@@ -0,0 +1,9 @@
+package com.vattima.lego.imaging.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(LegoImagingProperties.class)
+public class LegoImagingConfiguration {
+}
---
+++ src/test/java/com/vattima/lego/imaging/handler/KeywordsExtractorTest.java
@@ -0,0 +1,21 @@
+package com.vattima.lego.imaging.handler;
+
+import org.junit.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class KeywordsExtractorTest {
+
+    @Test
+    public void apply() throws Exception {
+        KeywordsExtractor keywordsExtractor = new KeywordsExtractor();
+
+
+
+        //Map<String, String> keywords = keywordsExtractor.apply()
+    }
+}
