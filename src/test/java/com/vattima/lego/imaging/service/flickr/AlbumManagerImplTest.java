b75f4c4c67d62f32232d7892c2c40a25979bb233
tvattima <github@vattima.com>
2019-04-07 21:13:30 -0400

    AlbumManagerImpl unit tests

--- src/test/java/com/vattima/lego/imaging/service/flickr/AlbumManagerImplTest.java
+++ src/test/java/com/vattima/lego/imaging/service/flickr/AlbumManagerImplTest.java
@@ -13,8 +13,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Comparator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -47,6 +52,8 @@ public class AlbumManagerImplTest {
                 .getAbsolutePath());
         bricklinkInventoryDao = mock(BricklinkInventoryDao.class);
 
+        deleteSubDirectoriesInPath(jpgPath);
+
         PhotoMetaData photoMetaData = new PhotoMetaData(jpgPath.resolve("DSC_0504.jpg"));
         AlbumManifest albumManifest = albumManager.addPhoto(photoMetaData);
         assertThat(albumManifest).isNotNull();
@@ -63,6 +70,8 @@ public class AlbumManagerImplTest {
                 .getAbsolutePath());
         bricklinkInventoryDao = mock(BricklinkInventoryDao.class);
 
+        deleteSubDirectoriesInPath(jpgPath);
+
         AlbumManager albumManager = new AlbumManagerImpl(imageManager, legoImagingProperties, imageCollector, flickrProperties, bricklinkInventoryDao);
         PhotoMetaData photoMetaData = new PhotoMetaData(jpgPath.resolve("DSC_0504-missing-uuid.jpg"));
         AlbumManifest albumManifest = albumManager.addPhoto(photoMetaData);
@@ -72,20 +81,43 @@ public class AlbumManagerImplTest {
 
     @Test
     public void addPhoto_inSameAlbum_returnsCachedAlbumManifest() throws Exception {
-        Path jpgPath = Paths.get(ResourceUtils.getURL("classpath:actual-lego-photos-with-keywords")
+        Path jpgPath = Paths.get(ResourceUtils.getURL("classpath:actual-lego-photos-with-keywords-cache-test")
                 .toURI());
         legoImagingProperties.setRootImagesFolder(jpgPath.toFile()
                 .getAbsolutePath());
         BricklinkInventoryDao bricklinkInventoryDao = mock(BricklinkInventoryDao.class);
+        deleteSubDirectoriesInPath(jpgPath);
 
-        ImageFileHolder imageFileHolder = new ImageFileHolder(Paths.get("."), imageCollector, bricklinkInventoryDao);
-        PhotoMetaData photoMetaData = new PhotoMetaData(jpgPath.resolve("DSC_0504.jpg"));
+        PhotoMetaData photoMetaData = new PhotoMetaData(jpgPath.resolve("DSC_0505.jpg"));
         AlbumManifest albumManifest1 = albumManager.addPhoto(photoMetaData);
         assertThat(albumManifest1).isNotNull();
 
-        photoMetaData = new PhotoMetaData(jpgPath.resolve("DSC_0505.jpg"));
+        photoMetaData = new PhotoMetaData(jpgPath.resolve("DSC_0506.jpg"));
         AlbumManifest albumManifest2 = albumManager.addPhoto(photoMetaData);
         assertThat(albumManifest2).isNotNull();
         assertThat(albumManifest2).isSameAs(albumManifest1);
+
+        photoMetaData = new PhotoMetaData(jpgPath.resolve("DSC_0514.jpg"));
+        AlbumManifest albumManifest3 = albumManager.addPhoto(photoMetaData);
+        assertThat(albumManifest1).isNotNull();
+        assertThat(albumManifest3).isNotIn(albumManifest1, albumManifest2);
+
+        photoMetaData = new PhotoMetaData(jpgPath.resolve("DSC_0515.jpg"));
+        AlbumManifest albumManifest4 = albumManager.addPhoto(photoMetaData);
+        assertThat(albumManifest4).isNotNull();
+        assertThat(albumManifest4).isNotIn(albumManifest1, albumManifest2);
+        assertThat(albumManifest4).isSameAs(albumManifest3);
+    }
+
+    private void deleteSubDirectoriesInPath(Path path) throws IOException {
+        Files.walk(path)
+                .filter(p -> Files.isDirectory(p, LinkOption.NOFOLLOW_LINKS))
+                .filter(p -> !p.equals(path))
+                .sorted(Comparator.reverseOrder())
+                .map(Path::toFile)
+                .forEach(p -> {
+                    final boolean deleted = p.delete();
+                    System.out.println("Delete ["+p+"] result --> ["+deleted+"]");
+                });
     }
 }
