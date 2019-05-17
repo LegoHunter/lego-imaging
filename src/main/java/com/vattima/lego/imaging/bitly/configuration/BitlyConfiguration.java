5eabadac6eb39585476d52411c9a3bf302e4d6f4
tvattima <github@vattima.com>
2019-05-16 22:44:49 -0400

    Enhancing Unit Tests for BitlyService

--- src/main/java/com/vattima/lego/imaging/bitly/configuration/BitlyConfiguration.java
+++ src/main/java/com/vattima/lego/imaging/bitly/configuration/BitlyConfiguration.java
@@ -1,5 +1,6 @@
 package com.vattima.lego.imaging.bitly.configuration;
 
+import com.vattima.lego.imaging.service.bitly.BitlyService;
 import net.swisstech.bitly.BitlyClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,7 +8,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class BitlyConfiguration {
     @Bean
-    public BitlyClient bitlyClient(BitlyProperties bitlyProperties) {
-        return new BitlyClient(bitlyProperties.getBitly().getAccessToken());
+    public BitlyClient bitlyClient(final BitlyProperties bitlyProperties) {
+        return new BitlyClient(bitlyProperties.getBitly()
+                                              .getAccessToken());
+    }
+
+    @Bean
+    public BitlyService bitlyService(final BitlyClient bitlyClient) {
+        return new BitlyService(bitlyClient);
     }
 }
--- src/test/java/com/vattima/lego/imaging/service/bitly/BitlyServiceTest.java
+++ src/test/java/com/vattima/lego/imaging/service/bitly/BitlyServiceTest.java
@@ -2,14 +2,17 @@ package com.vattima.lego.imaging.service.bitly;
 
 import lombok.extern.slf4j.Slf4j;
 import net.swisstech.bitly.BitlyClient;
+import net.swisstech.bitly.BitlyClientException;
 import net.swisstech.bitly.builder.v3.ShortenRequest;
 import net.swisstech.bitly.model.Response;
 import net.swisstech.bitly.model.v3.ShortenResponse;
 import org.junit.Test;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
@@ -30,6 +33,32 @@ public class BitlyServiceTest {
         assertThat(shortUrl).isEqualTo(new URL(shortUrlString));
     }
 
+    @Test
+    public void shorten_withNon200StatusCode_throwsException() throws Exception {
+        String longUrlString = "http://this.isalongurl.com/that/needs/to/be/shortened?a=123";
+
+        BitlyClient bitlyClient = setupMock(longUrlString, 500, "Something bad happened");
+        BitlyService bitlyService = new BitlyService(bitlyClient);
+
+        assertThatThrownBy(() -> {
+            bitlyService.shorten(new URL(longUrlString));
+        }).hasMessage("Bitly status [500] - Exception [Something bad happened]");
+    }
+
+    @Test
+    public void shorten_withMalformedUrl_throwsException() throws Exception {
+        String shortUrlString = "x://bit.ly/g7J4s20Lk";
+        String longUrlString = "http://this.isalongurl.com/that/needs/to/be/shortened?a=123";
+
+        BitlyClient bitlyClient = setupMock(longUrlString, shortUrlString);
+        BitlyService bitlyService = new BitlyService(bitlyClient);
+
+        assertThatThrownBy(() -> {
+            bitlyService.shorten(new URL(longUrlString));
+        }).hasRootCauseInstanceOf(MalformedURLException.class)
+          .hasMessageContaining("unknown protocol: x");
+    }
+
     private BitlyClient setupMock(final String longUrlString, final String shortUrlString) throws Exception {
         BitlyClient bitlyClient = mock(BitlyClient.class);
         ShortenRequest shortenRequest = mock(ShortenRequest.class);
@@ -39,9 +68,26 @@ public class BitlyServiceTest {
                                 .setLongUrl(eq(longUrlString));
         Response<ShortenResponse> response = new Response<>();
         response.status_code = 200;
+        response.status_txt = "OK";
         ShortenResponse shortenResponse = new ShortenResponse();
-        shortenResponse.url = new URL(shortUrlString).toExternalForm();
+        shortenResponse.url = shortUrlString;
         response.data = shortenResponse;
+        doReturn(response).when(shortenRequest)
+                          .call();
+        return bitlyClient;
+    }
+
+    private BitlyClient setupMock(final String longUrlString, final int status, final String errorMessage) throws Exception {
+        BitlyClient bitlyClient = mock(BitlyClient.class);
+        ShortenRequest shortenRequest = mock(ShortenRequest.class);
+        doReturn(shortenRequest).when(bitlyClient)
+                                .shorten();
+        doReturn(shortenRequest).when(shortenRequest)
+                                .setLongUrl(eq(longUrlString));
+        Response<ShortenResponse> response = new Response<>();
+        response.status_code = status;
+        response.status_txt = errorMessage;
+        response.data = null;
         doReturn(response).when(shortenRequest)
                           .call();
         return bitlyClient;
