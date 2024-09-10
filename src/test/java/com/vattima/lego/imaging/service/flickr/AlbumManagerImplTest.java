2a69c068269118ca39b2518fb7a400955a796984
tvatt <github@vattima.com>
2024-09-09 20:55:56 -0400

    Fixing unit tests to run on github

--- src/test/java/com/vattima/lego/imaging/service/flickr/AlbumManagerImplTest.java
+++ src/test/java/com/vattima/lego/imaging/service/flickr/AlbumManagerImplTest.java
@@ -18,12 +18,13 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.util.ResourceUtils;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 import java.util.stream.Stream;
 
+import static java.nio.file.FileVisitResult.CONTINUE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
@@ -92,6 +93,12 @@ class AlbumManagerImplTest {
     void addPhoto_inSameAlbum_returnsCachedAlbumManifest() throws Exception {
         Resource resource = new ClassPathResource("actual-lego-photos-with-keywords-cache-test");
         Path jpgPath = Paths.get(resource.getFile().toURI());
+
+
+        PrintFiles pf = new PrintFiles();
+        Files.walkFileTree(jpgPath, pf);
+
+
 
 //        Path jpgPath = PathUtils.fromClasspath("actual-lego-photos-with-keywords-cache-test");
 
@@ -179,5 +186,43 @@ class AlbumManagerImplTest {
                  photoMetaData.map(p -> assertThat(p).isIn(photos))
                               .orElseGet(() -> fail("Filename [" + pmd.getPath() + "] should have been found in AlbumManifest"));
              });
+    }
+
+    private static class PrintFiles extends SimpleFileVisitor<Path> {
+
+        // Print information about
+        // each type of file.
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attr) {
+            if (attr.isSymbolicLink()) {
+                System.out.format("Symbolic link: %s ", file);
+            } else if (attr.isRegularFile()) {
+                System.out.format("Regular file: %s ", file);
+            } else {
+                System.out.format("Other: %s ", file);
+            }
+            System.out.println("(" + attr.size() + "bytes)");
+            return CONTINUE;
+        }
+
+        // Print each directory visited.
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir,
+                                                  IOException exc) {
+            System.out.format("Directory: %s%n", dir);
+            return CONTINUE;
+        }
+
+        // If there is some error accessing
+        // the file, let the user know.
+        // If you don't override this method
+        // and an error occurs, an IOException
+        // is thrown.
+        @Override
+        public FileVisitResult visitFileFailed(Path file,
+                                               IOException exc) {
+            System.err.println(exc);
+            return CONTINUE;
+        }
     }
 }
