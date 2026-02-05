671a052a1d1111fd474e17bd218005c2fca0f21d
tvatt <github@vattima.com>
2026-02-04 22:37:32 -0500

    Commit and pushing local desktop work

--- src/main/java/com/vattima/lego/imaging/TestApplication.java
+++ src/main/java/com/vattima/lego/imaging/TestApplication.java
@@ -2,15 +2,14 @@ package com.vattima.lego.imaging;
 
 
 import com.vattima.lego.imaging.bitly.configuration.BitlyProperties;
+import io.legohunter.lego.data.configuration.SourceDataSourceProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.bricklink.data.lego.ibatis.configuration.DataSourceProperties;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @EnableConfigurationProperties
 @SpringBootApplication(scanBasePackages = {"net.bricklink", "com.bricklink", "com.vattima"})
@@ -23,7 +22,7 @@ public class TestApplication {
     @RequiredArgsConstructor
     @Slf4j
     static class PropertiesTest implements ApplicationRunner {
-        private final DataSourceProperties properties;
+        private final SourceDataSourceProperties properties;
         private final BitlyProperties bitlyProperties;
 
         @Override
