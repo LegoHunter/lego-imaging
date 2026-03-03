712762c8510e84260f50b61ce3343271d48c81b4
tvatt <github@vattima.com>
2026-03-03 10:02:22 -0500

    Updates unit test

--- src/main/java/com/vattima/lego/imaging/TestApplication.java
+++ src/main/java/com/vattima/lego/imaging/TestApplication.java
@@ -2,13 +2,13 @@ package com.vattima.lego.imaging;
 
 
 import com.vattima.lego.imaging.bitly.configuration.BitlyProperties;
-import io.legohunter.lego.data.configuration.SourceDataSourceProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @EnableConfigurationProperties
@@ -22,7 +22,7 @@ public class TestApplication {
     @RequiredArgsConstructor
     @Slf4j
     static class PropertiesTest implements ApplicationRunner {
-        private final SourceDataSourceProperties properties;
+        private final DataSourceProperties properties;
         private final BitlyProperties bitlyProperties;
 
         @Override
